3a280280ad8c6ccc5e74d6c7adb054c17b04570b
= <=>
2018-05-31 20:59:38 -0300

    Modularizando componentes

---
+++ essential/application/views/header.php
@@ -0,0 +1,78 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<style>
+
+			body
+			{
+
+				background: #cecece;
+			}
+			.head
+			{
+				height: 350px;
+				background-image: url('../static/img/bg-masthead.jpg');
+			}
+
+			nav{
+				padding-top: 25px;
+				height: 100px;
+			}
+
+			.drop
+			{
+				padding-left: -10px;
+				font-size: 40px;
+				text-align: center;
+				padding-top: 50px;
+				color: #fff;
+				position: relative;
+				height: 150px;
+				width: 150px;
+				background: teal;
+
+				border-radius: 75px 75px 75px 30px;
+			}
+
+			.col-md-12
+			{
+				margin-top: 25px;
+				
+				
+			}
+		</style>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+    <title>Lucas</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+		<nav>
+			<div class = "container">
+				<ul class="nav nav-pills">
+			  		<li role="presentation"><a href="#">Início</a></li>
+			  		<li role="presentation"><a href="#">Sobre</a></li>
+				  <li role="presentation"><a href="#">Contato</a></li>
+				</ul>
+			</div>
+		</nav>
--- essential/application/views/one.php
+++ essential/application/views/one.php
@@ -1,61 +1,67 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+include 'header.php';
 ?>
-<!DOCTYPE html>
-
-<html lang="en">
-
-	<head>
-
-		<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
-    <title>Lucas</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-		<nav>
-			<div class = "container">
-				<ul class="nav nav-pills">
-			  	<li role="presentation"><a href="#">Início</a></li>
-			  	<li role="presentation"><a href="#">Sobre</a></li>
-				</ul>
-			</div>
-		</nav>
-
-		<div class = "jumbotron">
+		<div class = "jumbotron head">
 			<section class="container">
-				<h2>Gabriel Santos</h2>
+				<h2>Gabriel Santos,</h2>
+				<h1>Santos Workers</h1>
 				<p>Designer, Ilustrador, Front-end Developer.</p>
 			</section>
 		</div>
 
-		<!-- main -->
+		<!-- main-->
 		<section class = "row">
 			<div class = "container">
+					
+					
+					<div align = "center"><h2><b>Experiência Profisisonal</b></h2></div>
+
+				<ul>
+					<li>Graduando em Engenharia da Computação, UFC, 2013.2</li>
+					<li>Concluíu os Estudos no Centro Educacional de Referência, 2012</li>
+					<li>Concluiu Curso de Inglês Intermediário, Palace, 2012</li>
+					<li>Aprendeu PHP, ontem, algum dia em 2018</li>
+					<li>Aprendeu JavaScript, Palace, 2011</li>
+					<li>Aprendeu Action Script, Palace, 2010</li>
+					<li>Aprendeu CSS, CSS Zen Garden, 2009</li>
+					<li>Aprendeu HTML, NCI, 2008</li>
+					<li>Nascido em Sobral, em 1995</li>
+				</ul>
+
+
+
+
+
+
+				<div align = "center"><h2><b>Experiência Profisisonal</b></h2></div>
 				<div class = "col-md-6">
-					<p><b>Designer</b></p>
-				</div>
-				<div class = "col-md-6">
-					<p><b>Developer</b></p>
+					<div class = "col-md-12">
+						<h3>Designerd!</h3>
+						<p>Desenvolve serviços com ilustrações personalizadas, desde ícones até mock-ups completos!</p>
+						<p class = "text-info">Santos Workers</p>
+					</div>
+					<div class = "col-md-12">
+						<h3>Design Patterns e Quality of Software </h3>
+						<p>Segue as melhores práticas de produção de Software</p>
+						<p class = "text-info">Fluent, LLC.</p>
+					</div>
+					<div class = "col-md-12">
+						<h3>iOS Developer </h3>
+						<p>Segue as melhores práticas de produção de Software</p>
+						<p class = "text-info">Foundation, Inc.</p>
+					</div>
+
 				</div>
 			</div>
+			
 		</section>
 
 		<!-- contato -->
 
-		<section class  = "container">
+		<!--section class  = "container">
 			<form>
 				<div class = "form-group">
 					<label for ="nome">Nome</label>
@@ -71,7 +77,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<input type = "submit" name = "submit" class = "btn btn-info">
 				</div>
 			</form>
-		</section>
+		</section-->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
